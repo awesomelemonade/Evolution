9e92030355ab75cfc6ad43849731f3a7dd0a2ee8
awesomelemonade <awesomelemonade@gmail.com>
2015-11-27 22:50:51 -0500

    Added Perspective to Toolbox

--- src/lemon/engine/toolbox/Toolbox.java
+++ src/lemon/engine/toolbox/Toolbox.java
@@ -10,6 +10,8 @@ import java.nio.IntBuffer;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.glfw.GLFW;
 
+import lemon.engine.math.Matrix;
+
 public class Toolbox {
 	private Toolbox(){}
 	
@@ -36,5 +38,17 @@ public class Toolbox {
 		IntBuffer height = BufferUtils.createIntBuffer(1);
 		GLFW.glfwGetWindowSize(window, width, height);
 		return ((float)width.get())/((float)height.get());
+	}
+	public static Matrix getPerspective(float fov, float aspect, float zNear, float zFar){
+		Matrix matrix = new Matrix(4);
+		float yScale = (float)(1/Math.tan(Math.toRadians(fov/2)));
+		float xScale = yScale/aspect;
+		matrix.set(0, 0, xScale);
+		matrix.set(1, 1, yScale);
+		matrix.set(2, 2, -(zNear+zFar)/(zFar-zNear));
+		matrix.set(2, 3, (-2*zNear*zFar)/(zFar-zNear));
+		matrix.set(3, 2, -1);
+		matrix.set(3, 3, 0);
+		return matrix;
 	}
 }
