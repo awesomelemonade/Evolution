9968112b05f677da5ab34836cc64cd1f9c001a4a
awesomelemonade <awesomelemonade@gmail.com>
2015-11-28 19:18:32 -0500

    Added Transformation Matrices to Toolbox

--- src/lemon/engine/toolbox/Toolbox.java
+++ src/lemon/engine/toolbox/Toolbox.java
@@ -11,6 +11,7 @@ import org.lwjgl.BufferUtils;
 import org.lwjgl.glfw.GLFW;
 
 import lemon.engine.math.Matrix;
+import lemon.engine.math.Vector;
 
 public class Toolbox {
 	private Toolbox(){}
@@ -49,6 +50,57 @@ public class Toolbox {
 		matrix.set(2, 3, (-2*zNear*zFar)/(zFar-zNear));
 		matrix.set(3, 2, -1);
 		matrix.set(3, 3, 0);
+		return matrix;
+	}
+	public static Matrix getTranslation(Vector vector){
+		Matrix matrix = Matrix.getIdentity(4);
+		matrix.set(0, 3, vector.getX());
+		matrix.set(1, 3, vector.getY());
+		matrix.set(2, 3, vector.getZ());
+		return matrix;
+	}
+	public static Matrix getRotationX(float angle){
+		Matrix matrix = new Matrix(4);
+		float sin = (float) Math.sin(Math.toRadians(angle));
+		float cos = (float) Math.cos(Math.toRadians(angle));
+		matrix.set(0, 0, 1);
+		matrix.set(1, 1, cos);
+		matrix.set(2, 1, sin);
+		matrix.set(1, 2, -sin);
+		matrix.set(2, 2, cos);
+		matrix.set(3, 3, 1);
+		return matrix;
+	}
+	public static Matrix getRotationY(float angle){
+		Matrix matrix = new Matrix(4);
+		float sin = (float) Math.sin(Math.toRadians(angle));
+		float cos = (float) Math.cos(Math.toRadians(angle));
+		matrix.set(0, 0, cos);
+		matrix.set(1, 1, 1);
+		matrix.set(2, 0, -sin);
+		matrix.set(0, 2, sin);
+		matrix.set(2, 2, cos);
+		matrix.set(3, 3, 1);
+		return matrix;
+	}
+	public static Matrix getRotationZ(float angle){
+		Matrix matrix = new Matrix(4);
+		float sin = (float) Math.sin(Math.toRadians(angle));
+		float cos = (float) Math.cos(Math.toRadians(angle));
+		matrix.set(0, 0, cos);
+		matrix.set(0, 1, sin);
+		matrix.set(1, 0, -sin);
+		matrix.set(1, 1, cos);
+		matrix.set(2, 2, 1);
+		matrix.set(3, 3, 1);
+		return matrix;
+	}
+	public static Matrix getScalar(Vector vector){
+		Matrix matrix = new Matrix(4);
+		matrix.set(0, 0, vector.getX());
+		matrix.set(1, 1, vector.getY());
+		matrix.set(2, 2, vector.getZ());
+		matrix.set(3, 3, 1);
 		return matrix;
 	}
 }
