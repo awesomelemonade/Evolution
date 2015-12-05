f1dcc832e3d873ee0bb712154ca93376fe1ad155
awesomelemonade <awesomelemonade@gmail.com>
2015-12-04 22:56:05 -0500

    Started Texture Package & Added BufferedImage to ByteBuffer to Toolbox

---
+++ src/lemon/engine/texture/Texture.java
@@ -0,0 +1,30 @@
+package lemon.engine.texture;
+
+import java.nio.ByteBuffer;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
+
+import lemon.engine.event.Listener;
+
+public class Texture implements Listener {
+	private int id;
+	public Texture(){
+		id = GL11.glGenTextures();
+	}
+	public void loadByteBuffer(int width, int height, ByteBuffer buffer){
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
+		//Wrap
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
+		//Scale
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+		//Send to OpenGL
+		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, width, height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
+	}
+	public int getId(){
+		return id;
+	}
+}
--- src/lemon/engine/toolbox/Toolbox.java
+++ src/lemon/engine/toolbox/Toolbox.java
@@ -1,10 +1,14 @@
 package lemon.engine.toolbox;
 
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import org.lwjgl.BufferUtils;
 
 public class Toolbox {
 	private Toolbox(){}
@@ -26,5 +30,21 @@ public class Toolbox {
 			e.printStackTrace();
 			return null;
 		}
+	}
+	public static ByteBuffer toByteBuffer(BufferedImage image){
+		int[] pixels = new int[image.getWidth()*image.getHeight()];
+		image.getRGB(0, 0, image.getWidth(), image.getHeight(), pixels, 0, image.getWidth());
+		ByteBuffer buffer = BufferUtils.createByteBuffer(image.getWidth()*image.getHeight()*4); //4=RGBA 3=RGB
+		for(int y=0;y<image.getHeight();++y){
+			for(int x=0;x<image.getWidth();++x){
+				int pixel = pixels[y*image.getWidth()+x];
+				buffer.put((byte)((pixel>>16)&0xFF)); //Red
+				buffer.put((byte)((pixel>>8)&0xFF)); //Green
+				buffer.put((byte)(pixel&0xFF)); //Blue
+				buffer.put((byte)((pixel>>24)&0xFF)); //Alpha
+			}
+		}
+		buffer.flip();
+		return buffer;
 	}
 }
