ebe768b37592df8964128d9b6be8cec91ad23dd2
awesomelemonade <awesomelemonade@gmail.com>
2015-12-05 16:38:44 -0500

    Added FrameBuffer and RenderBuffer #5

---
+++ src/lemon/engine/frameBuffer/FrameBuffer.java
@@ -0,0 +1,22 @@
+package lemon.engine.frameBuffer;
+
+import org.lwjgl.opengl.GL30;
+
+import lemon.engine.event.EventManager;
+import lemon.engine.event.Listener;
+import lemon.engine.event.Subscribe;
+
+public class FrameBuffer implements Listener {
+	private int id;
+	public FrameBuffer(){
+		id = GL30.glGenFramebuffers();
+		EventManager.INSTANCE.registerListener(this);
+	}
+	public int getId(){
+		return id;
+	}
+	@Subscribe
+	public void cleanUp(){
+		GL30.glDeleteFramebuffers(id);
+	}
+}
---
+++ src/lemon/engine/frameBuffer/RenderBuffer.java
@@ -0,0 +1,22 @@
+package lemon.engine.frameBuffer;
+
+import org.lwjgl.opengl.GL30;
+
+import lemon.engine.event.EventManager;
+import lemon.engine.event.Listener;
+import lemon.engine.event.Subscribe;
+
+public class RenderBuffer implements Listener {
+	private int id;
+	public RenderBuffer(){
+		id = GL30.glGenRenderbuffers();
+		EventManager.INSTANCE.registerListener(this);
+	}
+	public int getId(){
+		return id;
+	}
+	@Subscribe
+	public void cleanUp(){
+		GL30.glDeleteRenderbuffers(id);
+	}
+}
