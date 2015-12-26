4454bea98dcf92e17321790737bd757d41df7ad8
awesomelemonade <awesomelemonade@gmail.com>
2015-12-25 19:15:09 -0500

    Removed OldEventManager

--- src/lemon/engine/reflection/ReflectionUtil.java
+++ src/lemon/engine/reflection/ReflectionUtil.java
@@ -9,130 +9,75 @@ import java.net.URLClassLoader;
 public class ReflectionUtil {
 	private ReflectionUtil(){}
 	
-	public static Field getPrivateField(Class<?> clazz, String fieldName){
-		try {
-			return clazz.getDeclaredField(fieldName);
-		} catch (NoSuchFieldException | SecurityException e) {
-			return null;
-		}
+	public static Field getPrivateField(Class<?> clazz, String fieldName) throws NoSuchFieldException, SecurityException{
+		return clazz.getDeclaredField(fieldName);
 	}
-	public static Object getPrivateField(Class<?> clazz, String fieldName, Object object){
+	public static Object getPrivateField(Class<?> clazz, String fieldName, Object object) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException{
 		Field field = getPrivateField(clazz, fieldName);
 		try {
 			field.setAccessible(true);
 			return field.get(object);
-		} catch (IllegalArgumentException | IllegalAccessException e) {
-			return null;
 		} finally {
 			field.setAccessible(false);
 		}
 	}
-	public static Field getPublicField(Class<?> clazz, String fieldName){
-		try {
-			return clazz.getField(fieldName);
-		} catch (NoSuchFieldException | SecurityException e) {
-			return null;
-		}
+	public static Field getPublicField(Class<?> clazz, String fieldName) throws NoSuchFieldException, SecurityException{
+		return clazz.getField(fieldName);
 	}
-	public static Object getPublicField(Class<?> clazz, String fieldName, Object object){
+	public static Object getPublicField(Class<?> clazz, String fieldName, Object object) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException{
 		Field field = getPublicField(clazz, fieldName);
-		try {
-			return field.get(object);
-		} catch (IllegalArgumentException | IllegalAccessException e) {
-			return null;
-		}
+		return field.get(object);
 	}
-	public static boolean setPublicField(Object object, String fieldName, Object value){
-		Field field = ReflectionUtil.getPublicField(object.getClass(), fieldName);
-		try {
-			field.set(object, value);
-			return true;
-		} catch (IllegalArgumentException | IllegalAccessException e) {
-			return false;
-		}
+	public static void setPublicField(Object object, String fieldName, Object value) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException{
+		ReflectionUtil.getPublicField(object.getClass(), fieldName).set(object, value);
 	}
-	public static boolean setPrivateField(Object object, String fieldName, Object value){
+	public static boolean setPrivateField(Object object, String fieldName, Object value) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException{
 		Field field = ReflectionUtil.getPrivateField(object.getClass(), fieldName);
 		field.setAccessible(true);
 		try {
 			field.set(object, value);
 			return true;
-		} catch (IllegalArgumentException | IllegalAccessException e) {
-			return false;
-		} finally {
+		}finally {
 			field.setAccessible(false);
 		}
 	}
-	public static Method getPublicMethod(Class<?> clazz, String name, Class<?>... args){
-		try {
-			return clazz.getMethod(name, args);
-		} catch (NoSuchMethodException | SecurityException e) {
-			return null;
-		}
+	public static Method getPublicMethod(Class<?> clazz, String name, Class<?>... args) throws NoSuchMethodException, SecurityException{
+		return clazz.getMethod(name, args);
 	}
-	public static Method getPrivateMethod(Class<?> clazz, String name, Class<?>... args){
-		try {
-			return clazz.getDeclaredMethod(name, args);
-		} catch (NoSuchMethodException | SecurityException e) {
-			return null;
-		}
+	public static Method getPrivateMethod(Class<?> clazz, String name, Class<?>... args) throws NoSuchMethodException, SecurityException{
+		return clazz.getDeclaredMethod(name, args);
 	}
-	public static Object invokePublicMethod(Method method, Object object, Object... args){
-		try {
-			return method.invoke(object, args);
-		} catch (IllegalAccessException | IllegalArgumentException
-				| InvocationTargetException e) {
-			return null;
-		}
+	public static Object invokePublicMethod(Method method, Object object, Object... args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		return method.invoke(object, args);
 	}
-	public static Object invokePrivateMethod(Method method, Object object, Object... args){
+	public static Object invokePrivateMethod(Method method, Object object, Object... args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException{
 		try {
 			method.setAccessible(true);
 			return method.invoke(object, args);
-		} catch (IllegalAccessException | IllegalArgumentException
-				| InvocationTargetException e) {
-			return null;
 		} finally {
 			method.setAccessible(false);
 		}
 	}
-	public static <T> T getInstance(Class<T> clazz){
-		try {
-			return clazz.newInstance();
-		} catch (InstantiationException | IllegalAccessException e) {
-			return null;
-		}
+	public static <T> T getInstance(Class<T> clazz) throws InstantiationException, IllegalAccessException{
+		return clazz.newInstance();
 	}
-	public static Object getInstance(Class<?> clazz, Object... args){
+	public static Object getInstance(Class<?> clazz, Object... args) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
 		int numOfParams = 0;
 		if(args!=null){
 			numOfParams = args.length;
 		}
 		for(Constructor<?> constructor: clazz.getConstructors()){
 			if(constructor.getParameterTypes().length==numOfParams){
-				try {
-					return constructor.newInstance(args);
-				} catch (InstantiationException | IllegalAccessException
-						| IllegalArgumentException | InvocationTargetException e) {
-					return null;
-				}
+				return constructor.newInstance(args);
 			}
 		}
 		return null;
 	}
-	public static Class<?> getClass(String packageName, String className){
-		try {
-			return Class.forName(packageName+"."+className);
-		} catch (ClassNotFoundException e) {
-			return null;
-		}
+	public static Class<?> getClass(String packageName, String className) throws ClassNotFoundException{
+		return Class.forName(packageName+"."+className);
 	}
-	public static Class<?> getClass(String packageName, String className, URLClassLoader loader){
-		try {
-			return Class.forName(packageName+"."+className, true, loader);
-		} catch (ClassNotFoundException e) {
-			return null;
-		}
+	public static Class<?> getClass(String packageName, String className, URLClassLoader loader) throws ClassNotFoundException{
+		return Class.forName(packageName+"."+className, true, loader);
 	}
 	public static <T> T castObject(Class<T> clazz, Object object){
 		return clazz.cast(object);
