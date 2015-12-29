663d3a90ab5d08632f4b81b6ac466984bc0b2fc1
awesomelemonade <awesomelemonade@gmail.com>
2015-12-28 19:26:27 -0500

    Added Implementation of Entity Rendering System & Changed Initialization System

--- src/lemon/engine/entity/Quad.java
+++ src/lemon/engine/entity/Quad.java
@@ -1,15 +1,18 @@
 package lemon.engine.entity;
 
+import org.lwjgl.opengl.GL30;
+
 import lemon.engine.render.VertexArray;
 
 public class Quad implements Entity {
 	@Override
 	public void render() {
-		
+		GL30.glBindVertexArray(TestEntities.QUAD.getVertexArray().getId());
+		GL30.glBindVertexArray(0);
 	}
 	@Override
 	public VertexArray getVertexArray() {
-		return null;
+		return TestEntities.QUAD.getVertexArray();
 	}
 	@Override
 	public EntityType getType() {
--- src/lemon/engine/entity/TestEntities.java
+++ src/lemon/engine/entity/TestEntities.java
@@ -1,16 +1,45 @@
 package lemon.engine.entity;
 
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+
 import lemon.engine.control.UpdateEvent;
 import lemon.engine.event.EventManager;
 import lemon.engine.event.Listener;
 import lemon.engine.event.Subscribe;
 import lemon.engine.render.VertexArray;
+import lemon.engine.render.VertexArrayWatcher;
+import lemon.engine.toolbox.Toolbox;
 
-public enum TestEntities implements EntityType, Listener {
+public enum TestEntities implements EntityType, Listener, VertexArrayWatcher {
 	QUAD, TEST{
+		private VertexArray vertexArray;
 		@Override
 		public void init(){
 			EventManager.INSTANCE.registerListener(this);
+			vertexArray = new VertexArray();
+			GL30.glBindVertexArray(vertexArray.getId());
+			GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vertexArray.generateVbo().getId());
+			GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, Toolbox.toIntBuffer(
+					0, 1, 2, 1, 2, 3
+			), GL15.GL_STATIC_DRAW);
+			GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vertexArray.generateVbo().getId());
+			GL15.glBufferData(GL15.GL_ARRAY_BUFFER, Toolbox.toFloatBuffer(
+					-1f, 1f, 0f, 0f, 1f,
+					-1f, -1f, 0f, 0f, 0f,
+					1f, 1f, 0f, 1f, 1f,
+					1f, -1f, 0f, 1f, 0f
+			), GL15.GL_STATIC_DRAW);
+			GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 5*4, 0);
+			GL20.glVertexAttribPointer(1, 2, GL11.GL_FLOAT, false, 5*4, 3*4);
+			GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+			GL30.glBindVertexArray(0);
+		}
+		@Override
+		public VertexArray getVertexArray(){
+			return vertexArray;
 		}
 		//Some Random Event for Demonstration Purposes
 		@Subscribe
@@ -21,6 +50,10 @@ public enum TestEntities implements EntityType, Listener {
 	@Override
 	public void init() {
 		
+	}
+	@Override
+	public VertexArray getVertexArray(){
+		return null;
 	}
 	@Override
 	public String getName(){
