1fdffef735b78001d4e1f38a1ca3e890b6c4c15e
awesomelemonade <awesomelemonade@gmail.com>
2016-01-01 18:36:24 -0500

    Cleaned Up Unnecessary Code

--- src/lemon/engine/terrain/PerlinNoise.java
+++ src/lemon/engine/terrain/PerlinNoise.java
@@ -2,21 +2,19 @@ package lemon.engine.terrain;
 
 public class PerlinNoise {
 	private HashFunction hasher;
-	private PairingFunction pairer;
 	private float persistence;
 	private int iterations;
 	
-	public PerlinNoise(HashFunction hasher, PairingFunction pairer, float persistence, int iterations){
+	public PerlinNoise(HashFunction hasher, float persistence, int iterations){
 		this.hasher = hasher;
-		this.pairer = pairer;
 		this.persistence = persistence;
 		this.iterations = iterations;
 	}
 	public float noise(float x){
 		float output = 0;
 		for(int i=0;i<iterations;++i){
-			float frequency = (float)Math.pow(2, i); //pow too slow?
-			float amplitude = (float)Math.pow(persistence, i); //pow too slow?
+			float frequency = (float)Math.pow(2, i);
+			float amplitude = (float)Math.pow(persistence, i);
 			output+=get(x*frequency)*amplitude;
 		}
 		return output;
@@ -28,16 +26,11 @@ public class PerlinNoise {
 		float v2 = hash(intX+1);
 		return interpolate(v1, v2, fractionalX);
 	}
-	public float interpolate(float a, float b, float x){ //linear
+	public float interpolate(float a, float b, float x){
 		float ft = (float)(x*Math.PI);
 		float f = (float) ((1.0-Math.cos(ft)) * 0.5f);
 		return a*(1-f)+b*f;
 	}
-	/**
-	 * 
-	 * @param x
-	 * @return [-1 -> 1]
-	 */
 	public float hash(int x){
 		return ((float)hasher.hash(x))/((float)(Integer.MAX_VALUE));
 	}
--- src/lemon/engine/terrain/TerrainGenerator.java
+++ src/lemon/engine/terrain/TerrainGenerator.java
@@ -2,10 +2,9 @@ package lemon.engine.terrain;
 
 public class TerrainGenerator {
 	private PerlinNoise noise;
-	private static final PairingFunction pairer = new SzudzikPair();
 	
 	public TerrainGenerator(int seed){
-		noise = new PerlinNoise(new MurmurHash(seed), pairer, (float)(1.0/Math.sqrt(2)), 6);
+		noise = new PerlinNoise(new MurmurHash(seed), (float)(1.0/Math.sqrt(2)), 6);
 	}
 	public float generate(float x, float y){
 		return noise.noise((x/500f));
