f617fbd2cf5c43e78a33645d5183b63663356e98
awesomelemonade <awesomelemonade@gmail.com>
2016-01-13 18:22:55 -0500

    Added CubeMapData for SkyBox #24

---
+++ src/lemon/engine/texture/CubeMapData.java
@@ -0,0 +1,26 @@
+package lemon.engine.texture;
+
+import java.nio.ByteBuffer;
+
+public class CubeMapData {
+	private int width;
+	private int height;
+	private ByteBuffer[] data;
+	public CubeMapData(int width, int height, ByteBuffer[] data){
+		if(data.length!=6){
+			throw new IllegalArgumentException("ByteBuffer array has to be 6 length");
+		}
+		this.width = width;
+		this.height = height;
+		this.data = data;
+	}
+	public int getWidth(){
+		return width;
+	}
+	public int getHeight(){
+		return height;
+	}
+	public ByteBuffer[] getData(){
+		return data;
+	}
+}
--- src/lemon/engine/texture/Texture.java
+++ src/lemon/engine/texture/Texture.java
@@ -2,6 +2,7 @@ package lemon.engine.texture;
 
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
+import org.lwjgl.opengl.GL13;
 
 import lemon.engine.event.EventManager;
 import lemon.engine.event.Listener;
@@ -24,6 +25,19 @@ public class Texture implements Listener {
 		//Send to OpenGL
 		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, data.getWidth(), data.getHeight(), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, data.getData());
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
+	}
+	public void load(CubeMapData data){
+		GL11.glBindTexture(GL13.GL_TEXTURE_CUBE_MAP, id);
+		//Wrap
+		GL11.glTexParameteri(GL13.GL_TEXTURE_CUBE_MAP, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
+		GL11.glTexParameteri(GL13.GL_TEXTURE_CUBE_MAP, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
+		//Scale
+		GL11.glTexParameteri(GL13.GL_TEXTURE_CUBE_MAP, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+		GL11.glTexParameteri(GL13.GL_TEXTURE_CUBE_MAP, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+		for(int i=0;i<data.getData().length;++i){
+			GL11.glTexImage2D(GL13.GL_TEXTURE_CUBE_MAP_POSITIVE_X+i, 0, GL11.GL_RGBA, data.getWidth(), data.getHeight(), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, data.getData()[i]);
+		}
+		GL11.glBindTexture(GL13.GL_TEXTURE_CUBE_MAP, 0);
 	}
 	public int getId(){
 		return id;
