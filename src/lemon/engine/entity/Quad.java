036835b88e789be290e08a54b9fe39c40c8f15e6
awesomelemonade <awesomelemonade@gmail.com>
2016-02-05 08:39:56 -0500

    Improved Static Indices and Vertices Memory Usage

--- src/lemon/engine/entity/Quad.java
+++ src/lemon/engine/entity/Quad.java
@@ -1,5 +1,8 @@
 package lemon.engine.entity;
 
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
@@ -10,21 +13,21 @@ import lemon.engine.toolbox.Toolbox;
 
 public class Quad implements Entity {
 	private static VertexArray vertexArray;
-	private static final int VERTICES = 6;
+	private static final FloatBuffer VERTICES = Toolbox.toFloatBuffer(
+			-1f, 1f, 0f, 0f, 1f,
+			-1f, -1f, 0f, 0f, 0f,
+			1f, 1f, 0f, 1f, 1f,
+			1f, -1f, 0f, 1f, 0f
+	);
+	private static final IntBuffer INDICES = Toolbox.toIntBuffer(0, 1, 2, 1, 2, 3);
+	private static final int INDICES_COUNT = 6;
 	public static void init(){
 		vertexArray = new VertexArray();
 		GL30.glBindVertexArray(vertexArray.getId());
 		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vertexArray.generateVbo().getId());
-		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, Toolbox.toIntBuffer(
-				0, 1, 2, 1, 2, 3
-		), GL15.GL_STATIC_DRAW);
+		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, INDICES, GL15.GL_STATIC_DRAW);
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vertexArray.generateVbo().getId());
-		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, Toolbox.toFloatBuffer(
-				-1f, 1f, 0f, 0f, 1f,
-				-1f, -1f, 0f, 0f, 0f,
-				1f, 1f, 0f, 1f, 1f,
-				1f, -1f, 0f, 1f, 0f
-		), GL15.GL_STATIC_DRAW);
+		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, VERTICES, GL15.GL_STATIC_DRAW);
 		GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 5*4, 0);
 		GL20.glVertexAttribPointer(1, 2, GL11.GL_FLOAT, false, 5*4, 3*4);
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
@@ -35,7 +38,7 @@ public class Quad implements Entity {
 	@Override
 	public void render() {
 		GL30.glBindVertexArray(vertexArray.getId());
-		GL11.glDrawElements(GL11.GL_TRIANGLES, VERTICES, GL11.GL_UNSIGNED_INT, 0);
+		GL11.glDrawElements(GL11.GL_TRIANGLES, INDICES_COUNT, GL11.GL_UNSIGNED_INT, 0);
 		GL30.glBindVertexArray(0);
 	}
 	@Override
--- src/lemon/engine/entity/Skybox.java
+++ src/lemon/engine/entity/Skybox.java
@@ -1,5 +1,8 @@
 package lemon.engine.entity;
 
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
@@ -10,7 +13,7 @@ import lemon.engine.toolbox.Toolbox;
 
 public class Skybox implements Entity {
 	private static final float SIZE = 50f;
-	private static final float[] VERTICES = new float[]{
+	private static final FloatBuffer VERTICES = Toolbox.toFloatBuffer(
 			-SIZE, -SIZE, -SIZE,
 			-SIZE, -SIZE, SIZE,
 			-SIZE, SIZE, -SIZE,
@@ -19,23 +22,24 @@ public class Skybox implements Entity {
 			SIZE, -SIZE, SIZE,
 			SIZE, SIZE, -SIZE,
 			SIZE, SIZE, SIZE
-	};
-	private static final int[] INDICES = new int[]{
+	);
+	private static final IntBuffer INDICES = Toolbox.toIntBuffer(
 			2, 0, 4, 4, 6, 2,
 			1, 0, 2, 2, 3, 1,
 			4, 5, 7, 7, 6, 4,
 			1, 3, 7, 7, 5, 1,
 			2, 6, 7, 7, 3, 2,
 			0, 1, 4, 4, 1, 5
-	};
+	);
+	private static final int INDICES_COUNT = 36;
 	private static VertexArray vertexArray;
 	public static void init(){
 		vertexArray = new VertexArray();
 		GL30.glBindVertexArray(vertexArray.getId());
 		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vertexArray.generateVbo().getId());
-		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, Toolbox.toIntBuffer(INDICES), GL15.GL_STATIC_DRAW);
+		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, INDICES, GL15.GL_STATIC_DRAW);
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vertexArray.generateVbo().getId());
-		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, Toolbox.toFloatBuffer(VERTICES), GL15.GL_STATIC_DRAW);
+		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, VERTICES, GL15.GL_STATIC_DRAW);
 		GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 3*4, 0);
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
 		GL20.glEnableVertexAttribArray(0);
@@ -44,7 +48,7 @@ public class Skybox implements Entity {
 	@Override
 	public void render() {
 		GL30.glBindVertexArray(vertexArray.getId());
-		GL11.glDrawElements(GL11.GL_TRIANGLES, INDICES.length, GL11.GL_UNSIGNED_INT, 0);
+		GL11.glDrawElements(GL11.GL_TRIANGLES, INDICES_COUNT, GL11.GL_UNSIGNED_INT, 0);
 		GL30.glBindVertexArray(0);
 	}
 	@Override
