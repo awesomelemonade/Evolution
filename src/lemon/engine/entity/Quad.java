b97d2f79b89a335ad6e9ed73617ef97973a48325
awesomelemonade <awesomelemonade@gmail.com>
2016-02-08 16:13:19 -0500

    Converted Quad to Component Based Architecture

--- src/lemon/engine/entity/Quad.java
+++ src/lemon/engine/entity/Quad.java
@@ -11,8 +11,9 @@ import org.lwjgl.opengl.GL30;
 import lemon.engine.render.VertexArray;
 import lemon.engine.toolbox.Toolbox;
 
-public class Quad implements Entity {
-	private static VertexArray vertexArray;
+public class Quad {
+	private static final VertexArray VERTEX_ARRAY;
+	private static final Renderable RENDER_COMPONENT;
 	private static final FloatBuffer VERTICES = Toolbox.toFloatBuffer(
 			-1f, 1f, 0f, 0f, 1f,
 			-1f, -1f, 0f, 0f, 0f,
@@ -21,12 +22,12 @@ public class Quad implements Entity {
 	);
 	private static final IntBuffer INDICES = Toolbox.toIntBuffer(0, 1, 2, 1, 2, 3);
 	private static final int INDICES_COUNT = 6;
-	public static void init(){
-		vertexArray = new VertexArray();
-		GL30.glBindVertexArray(vertexArray.getId());
-		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vertexArray.generateVbo().getId());
+	static{
+		VERTEX_ARRAY = new VertexArray();
+		GL30.glBindVertexArray(VERTEX_ARRAY.getId());
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, VERTEX_ARRAY.generateVbo().getId());
 		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, INDICES, GL15.GL_STATIC_DRAW);
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vertexArray.generateVbo().getId());
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, VERTEX_ARRAY.generateVbo().getId());
 		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, VERTICES, GL15.GL_STATIC_DRAW);
 		GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 5*4, 0);
 		GL20.glVertexAttribPointer(1, 2, GL11.GL_FLOAT, false, 5*4, 3*4);
@@ -34,15 +35,18 @@ public class Quad implements Entity {
 		GL20.glEnableVertexAttribArray(0);
 		GL20.glEnableVertexAttribArray(1);
 		GL30.glBindVertexArray(0);
+		RENDER_COMPONENT = new Renderable(){
+			@Override
+			public VertexArray getVertexArray() {
+				return VERTEX_ARRAY;
+			}
+			@Override
+			public int getVertices() {
+				return INDICES_COUNT;
+			}
+		};
 	}
-	@Override
-	public void render() {
-		GL30.glBindVertexArray(vertexArray.getId());
-		GL11.glDrawElements(GL11.GL_TRIANGLES, INDICES_COUNT, GL11.GL_UNSIGNED_INT, 0);
-		GL30.glBindVertexArray(0);
-	}
-	@Override
-	public VertexArray getVertexArray() {
-		return vertexArray;
+	public Renderable getRenderComponent(){
+		return RENDER_COMPONENT;
 	}
 }
