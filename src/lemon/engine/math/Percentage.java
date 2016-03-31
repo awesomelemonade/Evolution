fd8f6ef094393c8c903a85d8ea4ec0d1cd5b53db
awesomelemonade <awesomelemonade@gmail.com>
2016-03-31 17:10:27 -0400

    Added Percentage to Math

---
+++ src/lemon/engine/math/Percentage.java
@@ -0,0 +1,25 @@
+package lemon.engine.math;
+
+public class Percentage {
+	private int part;
+	private int whole;
+	public Percentage(int part, int whole){
+		this.part = part;
+		this.whole = whole;
+	}
+	public void setPart(int part) {
+		this.part = part;
+	}
+	public int getPart() {
+		return part;
+	}
+	public void setWhole(int whole) {
+		this.whole = whole;
+	}
+	public int getWhole() {
+		return whole;
+	}
+	public float getPercentage(){
+		return ((float)part)/((float)whole);
+	}
+}
