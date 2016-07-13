503a074402d6225115f1975d0c27c801f2bc7cd1
awesomelemonade <awesomelemonade@gmail.com>
2016-07-12 23:19:51 -0400

    Added Loading Screen to Events

--- src/lemon/engine/evolution/Loading.java
+++ src/lemon/engine/evolution/Loading.java
@@ -4,6 +4,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import lemon.engine.control.Loader;
+import lemon.engine.control.RenderEvent;
+import lemon.engine.control.UpdateEvent;
 import lemon.engine.control.WindowInitEvent;
 import lemon.engine.event.EventManager;
 import lemon.engine.event.Listener;
@@ -12,19 +14,37 @@ import lemon.engine.event.Subscribe;
 public enum Loading implements Listener {
 	INSTANCE;
 	private static final Logger logger = Logger.getLogger(Game.class.getName());
+	private Loader loader;
+	private long window;
+	private boolean started;
 	@Subscribe
-	public void init(WindowInitEvent event){
-		Loader loader = Game.INSTANCE.getTerrainLoader();
+	public void load(WindowInitEvent event){
+		this.loader = Game.INSTANCE.getTerrainLoader();
 		loader.load();
-		while(loader.getPercentage().getPart()!=loader.getPercentage().getWhole()){
-			logger.log(Level.INFO, loader.getPercentage().getPercentage()+"%");
-			try{
-				Thread.sleep(1000);
-			}catch(Exception ex){
-				ex.printStackTrace();
-			}
+		this.window = event.getWindow();
+		started = false;
+	}
+	@Subscribe
+	public void update(UpdateEvent event){
+		if(started){
+			return;
 		}
-		Game.INSTANCE.init(event.getWindow());
+		if(loader.getPercentage().getPart()!=loader.getPercentage().getWhole()){
+			logger.log(Level.INFO, loader.getPercentage().getPercentage()+"%");
+		}else{
+			start(window);
+		}
+	}
+	@Subscribe
+	public void render(RenderEvent event){
+		if(started){
+			return;
+		}
+		
+	}
+	public void start(long window){
+		Game.INSTANCE.init(window);
 		EventManager.INSTANCE.registerListener(Game.INSTANCE);
+		started = true;
 	}
 }
