86c070e91e828b8907c20c222e09be7fc3c85acc
awesomelemonade <awesomelemonade@gmail.com>
2016-07-30 23:56:18 -0400

    Setup Rendering System in Loading Screen

--- src/lemon/engine/evolution/Loading.java
+++ src/lemon/engine/evolution/Loading.java
@@ -3,6 +3,8 @@ package lemon.engine.evolution;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.lwjgl.opengl.GL20;
+
 import lemon.engine.control.Loader;
 import lemon.engine.control.RenderEvent;
 import lemon.engine.control.UpdateEvent;
@@ -10,15 +12,25 @@ import lemon.engine.control.WindowInitEvent;
 import lemon.engine.event.EventManager;
 import lemon.engine.event.Listener;
 import lemon.engine.event.Subscribe;
+import lemon.engine.render.Shader;
+import lemon.engine.render.ShaderProgram;
+import lemon.engine.toolbox.Toolbox;
 
 public enum Loading implements Listener {
 	INSTANCE;
 	private static final Logger logger = Logger.getLogger(Game.class.getName());
+	private ShaderProgram shaderProgram;
 	private Loader loader;
 	private long window;
 	private boolean started;
 	@Subscribe
 	public void load(WindowInitEvent event){
+		shaderProgram = new ShaderProgram(
+				new int[]{0, 1},
+				new String[]{"position", "color"},
+				new Shader(GL20.GL_VERTEX_SHADER, Toolbox.getFile("shaders2d/colorVertexShader")),
+				new Shader(GL20.GL_FRAGMENT_SHADER, Toolbox.getFile("shaders2d/colorFragmentShader"))
+		);
 		this.loader = Game.INSTANCE.getTerrainLoader();
 		loader.load();
 		this.window = event.getWindow();
@@ -40,7 +52,9 @@ public enum Loading implements Listener {
 		if(started){
 			return;
 		}
+		GL20.glUseProgram(shaderProgram.getId());
 		
+		GL20.glUseProgram(0);
 	}
 	public void start(long window){
 		Game.INSTANCE.init(window);
