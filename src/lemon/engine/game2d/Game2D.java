c472cbc57c226e7519faf43dff1f1791130f6c04
awesomelemonade <awesomelemonade@gmail.com>
2016-08-06 12:22:50 -0400

    Added Player2D

--- src/lemon/engine/game2d/Game2D.java
+++ src/lemon/engine/game2d/Game2D.java
@@ -12,12 +12,13 @@ import lemon.engine.control.WindowInitEvent;
 
 import lemon.engine.event.Listener;
 import lemon.engine.event.Subscribe;
+import lemon.engine.game.PlayerControls;
+import lemon.engine.game.StandardControls;
 import lemon.engine.math.MathUtil;
 import lemon.engine.math.Matrix;
 import lemon.engine.render.Shader;
 import lemon.engine.render.ShaderProgram;
 import lemon.engine.render.UniformVariable;
-import lemon.engine.toolbox.Color;
 import lemon.engine.toolbox.Toolbox;
 
 public enum Game2D implements Listener {
@@ -25,8 +26,14 @@ public enum Game2D implements Listener {
 	private ShaderProgram shaderProgram;
 	private UniformVariable	uniform_projectionMatrix;
 	private UniformVariable uniform_transformationMatrix;
-	private Quad2D player;
 	private Matrix projectionMatrix;
+	
+	private static final float PLAYER_SPEED = 5f;
+	private static final float PLAYER_FRICTION = 0.5f;
+	private static final float PLAYER_DELTA_MODIFIER = 0.00000001f;
+	private Player2D player;
+	private PlayerControls<Integer, Integer> controls;
+	
 	@Subscribe
 	public void load(WindowInitEvent event){
 		IntBuffer width = BufferUtils.createIntBuffer(1);
@@ -47,15 +54,38 @@ public enum Game2D implements Listener {
 		uniform_projectionMatrix.loadMatrix(projectionMatrix);
 		uniform_transformationMatrix.loadMatrix(Matrix.getIdentity(4));
 		GL20.glUseProgram(0);
-		player = new Quad2D(new Box2D(0f, 0, 20f, 50f), new Color(1f, 0f, 0f));
+		
+		player = new Player2D();
+		controls = new StandardControls();
+		controls.bindKey(GLFW.GLFW_KEY_A, GLFW.GLFW_KEY_A);
+		controls.bindKey(GLFW.GLFW_KEY_D, GLFW.GLFW_KEY_D);
+		controls.bindKey(GLFW.GLFW_KEY_W, GLFW.GLFW_KEY_W);
+		controls.bindKey(GLFW.GLFW_KEY_S, GLFW.GLFW_KEY_S);
 	}
 	@Subscribe
 	public void update(UpdateEvent event){
-		
+		if(controls.hasStates()){
+			if(controls.getState(GLFW.GLFW_KEY_W)){
+				player.getVelocity().setY(player.getVelocity().getY()+PLAYER_SPEED*event.getDelta()*PLAYER_DELTA_MODIFIER);
+			}
+			if(controls.getState(GLFW.GLFW_KEY_A)){
+				player.getVelocity().setX(player.getVelocity().getX()-PLAYER_SPEED*event.getDelta()*PLAYER_DELTA_MODIFIER);
+			}
+			if(controls.getState(GLFW.GLFW_KEY_S)){
+				player.getVelocity().setY(player.getVelocity().getY()-PLAYER_SPEED*event.getDelta()*PLAYER_DELTA_MODIFIER);
+			}
+			if(controls.getState(GLFW.GLFW_KEY_D)){
+				player.getVelocity().setX(player.getVelocity().getX()+PLAYER_SPEED*event.getDelta()*PLAYER_DELTA_MODIFIER);
+			}
+		}
+		player.getVelocity().setX((float) (player.getVelocity().getX()*Math.pow(PLAYER_FRICTION, event.getDelta()*PLAYER_DELTA_MODIFIER)));
+		player.getVelocity().setY((float) (player.getVelocity().getY()*Math.pow(PLAYER_FRICTION, event.getDelta()*PLAYER_DELTA_MODIFIER)));
+		player.update(event);
 	}
 	@Subscribe
 	public void render(RenderEvent event){
 		GL20.glUseProgram(shaderProgram.getId());
+		uniform_transformationMatrix.loadMatrix(MathUtil.getTranslation(player.getPosition()));
 		player.render();
 		GL20.glUseProgram(0);
 	}
---
+++ src/lemon/engine/game2d/Player2D.java
@@ -0,0 +1,33 @@
+package lemon.engine.game2d;
+
+import lemon.engine.control.UpdateEvent;
+import lemon.engine.math.Vector;
+import lemon.engine.render.Renderable;
+import lemon.engine.toolbox.Color;
+
+public class Player2D implements Renderable {
+	private static final float DELTA_MODIFIER = 0.0000001f;
+	private Vector position;
+	private Vector velocity;
+	private Quad2D quad;
+	public Player2D(){
+		quad = new Quad2D(new Box2D(0f, 0, 20f, 50f), new Color(1f, 0f, 0f));
+		position = new Vector();
+		velocity = new Vector();
+	}
+	public void update(UpdateEvent event){
+		position.setX(position.getX()+velocity.getX()*event.getDelta()*DELTA_MODIFIER);
+		position.setY(position.getY()+velocity.getY()*event.getDelta()*DELTA_MODIFIER);
+		position.setZ(position.getZ()+velocity.getZ()*event.getDelta()*DELTA_MODIFIER);
+	}
+	@Override
+	public void render(){
+		quad.render();
+	}
+	public Vector getPosition(){
+		return position;
+	}
+	public Vector getVelocity(){
+		return velocity;
+	}
+}
