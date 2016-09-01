d20a6a16836768007d44128f50a9613200949a63
awesomelemonade <awesomelemonade@gmail.com>
2016-09-01 01:28:33 -0400

    Added Constructors to Line and Triangle

--- src/lemon/engine/math/Line.java
+++ src/lemon/engine/math/Line.java
@@ -4,4 +4,9 @@ public class Line extends VectorArray {
 	public Line() {
 		super(2);
 	}
+	public Line(Vector vector, Vector vector2){
+		this();
+		this.set(0, vector);
+		this.set(1, vector2);
+	}
 }
--- src/lemon/engine/math/Triangle.java
+++ src/lemon/engine/math/Triangle.java
@@ -4,4 +4,10 @@ public class Triangle extends VectorArray {
 	public Triangle() {
 		super(3);
 	}
+	public Triangle(Vector vector, Vector vector2, Vector vector3){
+		this();
+		this.set(0, vector);
+		this.set(1, vector2);;
+		this.set(2, vector3);
+	}
 }
