dd1b4a7ab227f17adbc98445a574d84ece062d3f
awesomelemonade <awesomelemonade@gmail.com>
2016-08-31 23:23:09 -0400

    Added 2D Constructor to Vector

--- src/lemon/engine/math/Vector.java
+++ src/lemon/engine/math/Vector.java
@@ -16,7 +16,10 @@ public class Vector {
 	private float y;
 	private float z;
 	public Vector(){
-		this(0f, 0f, 0f);
+		this(0f, 0f);
+	}
+	public Vector(float x, float y){
+		this(x, y, 0f);
 	}
 	public Vector(float x, float y, float z){
 		this.x = x;
