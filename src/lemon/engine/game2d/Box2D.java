332b4fb5b048361b1e94f5a5b475f62c6f09f88d
awesomelemonade <awesomelemonade@gmail.com>
2016-12-28 12:43:37 -0500

    Vectorized Box2D

--- src/lemon/engine/game2d/Box2D.java
+++ src/lemon/engine/game2d/Box2D.java
@@ -2,52 +2,46 @@ package lemon.engine.game2d;
 
 import lemon.engine.math.MathUtil;
 import lemon.engine.math.Matrix;
+import lemon.engine.math.Vector;
 import lemon.engine.math.Vector3D;
 
-public class Box2D {
-	private float x;
-	private float y;
-	private float width;
-	private float height;
+public class Box2D extends Vector {
 	public Box2D(float x, float y, float width, float height){
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
+		super(x, y, width, height);
 	}
 	public boolean intersect(float x, float y){
-		return x>this.x&&x<this.x+this.width&&y>this.y&&y<this.y+this.height;
+		return x>this.getX()&&x<this.getX()+this.getWidth()&&y>this.getY()&&y<this.getY()+this.getHeight();
 	}
 	public void setX(float x){
-		this.x = x;
+		super.set(0, x);
 	}
 	public float getX(){
-		return x;
+		return super.get(0);
 	}
 	public void setY(float y){
-		this.y = y;
+		super.set(1, y);
 	}
 	public float getY(){
-		return y;
+		return super.get(1);
 	}
 	public void setWidth(float width){
-		this.width = width;
+		super.set(2, width);
 	}
 	public float getWidth(){
-		return width;
+		return super.get(2);
 	}
 	public void setHeight(float height){
-		this.height = height;
+		super.set(3, height);
 	}
 	public float getHeight(){
-		return height;
+		return super.get(3);
 	}
 	public Matrix getTransformationMatrix(){
-		return MathUtil.getTranslation(new Vector3D(x+(width/2f), y+(height/2f), 0f))
-				.multiply(MathUtil.getScalar(new Vector3D(width/2f, height/2f, 1f)));
+		return MathUtil.getTranslation(new Vector3D(this.getX()+(this.getWidth()/2f), this.getY()+(this.getHeight()/2f), 0f))
+				.multiply(MathUtil.getScalar(new Vector3D(this.getWidth()/2f, this.getHeight()/2f, 1f)));
 	}
 	@Override
 	public String toString(){
-		return String.format("Box2D[%f, %f, %f, %f]", x, y, width, height);
+		return String.format("Box2D[%f, %f, %f, %f]", this.getX(), this.getY(), this.getWidth(), this.getHeight());
 	}
 }
