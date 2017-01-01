7126f97d35a08737ca314fff2e4f5c46d02b6aee
awesomelemonade <awesomelemonade@gmail.com>
2017-01-01 16:48:17 -0500

    Added NHDScene

---
+++ src/lemon/engine/game2d/NHDScene.java
@@ -0,0 +1,166 @@
+package lemon.engine.game2d;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.IntBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import javax.imageio.ImageIO;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+
+import lemon.engine.animation.FunctionInterpolator;
+import lemon.engine.animation.Interpolator;
+import lemon.engine.control.RenderEvent;
+import lemon.engine.control.UpdateEvent;
+import lemon.engine.entity.Quad;
+import lemon.engine.event.EventManager;
+import lemon.engine.event.Listener;
+import lemon.engine.event.Subscribe;
+import lemon.engine.evolution.BezierCurves;
+import lemon.engine.evolution.CommonPrograms2D;
+import lemon.engine.evolution.SplitScreen;
+import lemon.engine.input.KeyEvent;
+import lemon.engine.math.MathUtil;
+import lemon.engine.math.Matrix;
+import lemon.engine.math.Vector;
+import lemon.engine.render.MatrixType;
+import lemon.engine.texture.Texture;
+import lemon.engine.texture.TextureBank;
+import lemon.engine.texture.TextureData;
+
+public enum NHDScene implements Listener {
+	INSTANCE;
+	private Matrix projectionMatrix;
+	private Map<String, Texture> textures;
+	
+	private Box2D windowBox;
+	
+	private SplitScreen main;
+	
+	
+	private List<Interpolator> interpolators;
+	
+	public void start(long window){
+		IntBuffer width = BufferUtils.createIntBuffer(1);
+		IntBuffer height = BufferUtils.createIntBuffer(1);
+		GLFW.glfwGetWindowSize(window, width, height);
+		int window_width = width.get();
+		int window_height = height.get();
+		windowBox = new Box2D(0, 0, window_width, window_height);
+		projectionMatrix = MathUtil.getOrtho(window_width, window_height, -1f, 1f);
+		
+		//projectionMatrix = MathUtil.getOrtho(window_width*2f, window_height*2f, -1f, 1f);
+		//projectionMatrix = projectionMatrix.multiply(MathUtil.getTranslation(new Vector3D(window_width/2, window_height/2, 0)));
+		
+		main = new SplitScreen((int)windowBox.getWidth(), (int)windowBox.getHeight());
+		
+		GL20.glUseProgram(CommonPrograms2D.COLOR.getShaderProgram().getId());
+		CommonPrograms2D.COLOR.getShaderProgram().loadMatrix(MatrixType.PROJECTION_MATRIX, projectionMatrix);
+		CommonPrograms2D.COLOR.getShaderProgram().loadMatrix(MatrixType.VIEW_MATRIX, Matrix.IDENTITY_4);
+		CommonPrograms2D.COLOR.getShaderProgram().loadMatrix(MatrixType.MODEL_MATRIX, Matrix.IDENTITY_4);
+		GL20.glUseProgram(0);
+		GL20.glUseProgram(CommonPrograms2D.TEXTURE.getShaderProgram().getId());
+		CommonPrograms2D.TEXTURE.getShaderProgram().loadMatrix(MatrixType.PROJECTION_MATRIX, projectionMatrix);
+		CommonPrograms2D.TEXTURE.getShaderProgram().loadMatrix(MatrixType.VIEW_MATRIX, Matrix.IDENTITY_4);
+		CommonPrograms2D.TEXTURE.getShaderProgram().loadMatrix(MatrixType.MODEL_MATRIX, Matrix.IDENTITY_4);
+		CommonPrograms2D.TEXTURE.getShaderProgram().loadInt("textureSampler", TextureBank.REUSE.getId());
+		GL20.glUseProgram(0);
+		textures = new HashMap<String, Texture>();
+		String resFolder = "res/"+this.getClass().getSimpleName()+"/";
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(new File(resFolder+"texture-config")));
+			String line;
+			while((line=reader.readLine())!=null){
+				StringTokenizer tokenizer = new StringTokenizer(line);
+				String name = tokenizer.nextToken();
+				System.out.println("Loading: "+name);
+				Texture texture = new Texture();
+				texture.load(new TextureData(ImageIO.read(new File(resFolder+tokenizer.nextToken()))));
+				textures.put(name, texture);
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		interpolators = new ArrayList<Interpolator>();
+		
+		
+		EventManager.INSTANCE.registerListener(this);
+	}
+	long time = -500000000;
+	@Subscribe
+	public void update(UpdateEvent event){
+		time+=event.getDelta();
+		for(Interpolator interpolator: interpolators){
+			interpolator.update(time);
+		}
+	}
+	@Subscribe
+	public void render(RenderEvent event){
+		
+		
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		
+		
+		
+		GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, main.getFrameBuffer().getId());
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+		GL20.glUseProgram(CommonPrograms2D.TEXTURE.getShaderProgram().getId());
+		
+		
+		
+		GL20.glUseProgram(0);
+		
+		GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, 0);
+		
+		
+		main.render(windowBox, windowBox);
+		
+		GL11.glDisable(GL11.GL_BLEND);
+	}
+	public long getTime(long milliseconds){
+		return milliseconds*1000000;
+	}
+	public float getTimeProgress(long time, long startTime, long endTime){
+		if(time<=startTime){
+			return 0f;
+		}
+		if(time>=endTime){
+			return 1f;
+		}
+		return ((float)(time-startTime))/((float)(endTime-startTime));
+	}
+	public float getProgress(float current, float start, float finish){
+		if(current<=start){
+			return 0f;
+		}
+		if(current>=finish){
+			return 1f;
+		}
+		return ((float)(current-start))/((float)(finish-start));
+	}
+	@Subscribe
+	public void onKey(KeyEvent event){
+		if(event.getAction()==GLFW.GLFW_PRESS){
+			
+		}
+		if(event.getAction()==GLFW.GLFW_RELEASE){
+			if(event.getKey()==GLFW.GLFW_KEY_R){
+				time = -500000000;
+			}
+		}
+	}
+}
