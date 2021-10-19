dbef88049d0fb4d720db5f6ec6688c32e9b648f1
ianh729 <87191360+ianh729@users.noreply.github.com>
2021-10-19 10:07:54 -0400

    added event to UIImage, added path method to ItemType interface to return different images for each item

--- src/main/java/lemon/evolution/item/RocketLauncherItemType.java
+++ src/main/java/lemon/evolution/item/RocketLauncherItemType.java
@@ -1,6 +1,7 @@
 package lemon.evolution.item;
 
 import lemon.engine.game.Player;
+import lemon.engine.texture.Texture;
 import lemon.evolution.entity.RocketLauncherProjectile;
 import lemon.evolution.world.ControllableEntity;
 
@@ -11,11 +12,17 @@ public enum RocketLauncherItemType implements ItemType {
 		return "Rocket Launcher";
 	}
 
+	private final String imagePath2D = "/res/China.png";
+
 	@Override
 	public void use(ControllableEntity player) {
 		player.world().entities().add(new RocketLauncherProjectile(
 				player.location().add(player.vectorDirection().multiply(0.5f)),
 				player.vectorDirection().multiply(5f)
 		));
+	}
+
+	public String guiImagePath() {
+		return "/res/inventory_icons/China.png";
 	}
 }
