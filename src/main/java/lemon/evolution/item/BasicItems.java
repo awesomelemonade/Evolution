e653075acf7db3490b75669a682d232606101eac
awesomelemonade <awesomelemonade@gmail.com>
2021-10-31 16:38:32 -0400

    Moved newLaunchedEntity() to EntityUtil

--- src/main/java/lemon/evolution/item/BasicItems.java
+++ src/main/java/lemon/evolution/item/BasicItems.java
@@ -1,26 +1,24 @@
 package lemon.evolution.item;
 
 import lemon.engine.math.Vector3D;
+import lemon.evolution.entity.EntityUtil;
 import lemon.evolution.entity.ExplodeOnHitProjectile;
 import lemon.evolution.entity.ExplodeOnTimeProjectile;
 import lemon.evolution.entity.MissileShowerEntity;
 import lemon.evolution.world.ControllableEntity;
-import lemon.evolution.world.Entity;
-import lemon.evolution.world.Location;
 
 import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
 
 public enum BasicItems implements ItemType {
     ROCKET_LAUNCHER("Rocket Launcher", "/res/inventory_icons/rocketLauncher.png", (player, power) -> {
-        newLaunchedEntity(player, power, (location, velocity) ->
+        EntityUtil.newLaunchedEntity(player, power, (location, velocity) ->
                 new ExplodeOnHitProjectile(location, velocity, ExplodeOnHitProjectile.Type.MISSILE));
     }),
     MISSILE_SHOWER("Missile Shower", "/res/inventory_icons/missile.png", (player, power) -> {
-        newLaunchedEntity(player, power, MissileShowerEntity::new);
+        EntityUtil.newLaunchedEntity(player, power, MissileShowerEntity::new);
     }),
     GRENADE_LAUNCHER("Grenade Launcher", "/res/inventory_icons/rocketLauncher.png", (player, power) -> {
-        newLaunchedEntity(player, power / 3f, (location, velocity) ->
+        EntityUtil.newLaunchedEntity(player, power / 3f, (location, velocity) ->
                 new ExplodeOnTimeProjectile(location, velocity, ExplodeOnTimeProjectile.Type.GRENADE));
     });
     private final String name;
@@ -46,16 +44,5 @@ public enum BasicItems implements ItemType {
     @Override
     public String guiImagePath() {
         return guiImagePath;
-    }
-
-    private static void newLaunchedEntity(ControllableEntity player, float power, BiFunction<Location, Vector3D, Entity> constructor) {
-        player.world().entities().add(constructor.apply(
-                getPlayerOffset(player, 0.5f),
-                player.vectorDirection().multiply(5f * power)
-        ));
-    }
-
-    private static Location getPlayerOffset(ControllableEntity player, float offset) {
-        return player.location().add(player.vectorDirection().multiply(offset));
     }
 }
