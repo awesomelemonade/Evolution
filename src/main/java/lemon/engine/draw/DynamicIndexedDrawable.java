55c10e8be64739f5370a85043fd4438191ba2d3c
awesomelemonade <awesomelemonade@gmail.com>
2021-11-13 17:57:40 -0500

    Offload computation of float buffer into background pool

--- src/main/java/lemon/engine/draw/DynamicIndexedDrawable.java
+++ src/main/java/lemon/engine/draw/DynamicIndexedDrawable.java
@@ -1,6 +1,5 @@
 package lemon.engine.draw;
 
-import lemon.engine.math.FloatData;
 import lemon.engine.render.VertexArray;
 import lemon.engine.render.VertexBuffer;
 import org.lwjgl.opengl.GL11;
@@ -12,7 +11,7 @@ import java.nio.FloatBuffer;
 public class DynamicIndexedDrawable implements Drawable {
 	private VertexArray vertexArray = null;
 	private int numIndices;
-	private int stride;
+	private int currentStride;
 	private int drawMode;
 	private VertexBuffer indexBuffer;
 	private int indexBufferSize;
@@ -20,70 +19,73 @@ public class DynamicIndexedDrawable implements Drawable {
 	private int vertexBufferSize;
 	private int hint;
 
-	public DynamicIndexedDrawable(int[] indices, FloatData[][] vertices) {
-		this(indices, vertices, GL11.GL_TRIANGLES, GL15.GL_DYNAMIC_DRAW);
+	public DynamicIndexedDrawable(DrawableData data) {
+		this(data, GL11.GL_TRIANGLES, GL15.GL_DYNAMIC_DRAW);
 	}
 
-	public DynamicIndexedDrawable(int[] indices, FloatData[][] vertices, int drawMode, int hint) {
-		this.numIndices = indices.length;
+	public DynamicIndexedDrawable(DrawableData data, int drawMode, int hint) {
 		this.drawMode = drawMode;
 		this.hint = hint;
-		this.stride = Drawable.getStride(vertices);
+		initVertexArray(data);
+	}
+
+	private void initVertexArray(DrawableData data) {
+		this.numIndices = data.indices().length;
 		if (numIndices > 0) {
-			initVertexArray(indices, vertices);
+			currentStride = data.stride();
+			vertexArray = new VertexArray();
+			vertexArray.bind(vao -> {
+				indexBuffer = new VertexBuffer();
+				indexBuffer.bind(GL15.GL_ELEMENT_ARRAY_BUFFER, (target, vbo) -> {
+					this.indexBufferSize = numIndices;
+					GL15.glBufferData(target, data.indices(), hint);
+				}, false);
+				var vertices = data.vertices();
+				vertexBuffer = new VertexBuffer();
+				vertexBuffer.bind(GL15.GL_ARRAY_BUFFER, (target, vbo) -> {
+					FloatBuffer buffer = Drawable.getFloatBuffer(vertices, currentStride);
+					this.vertexBufferSize = buffer.capacity();
+					GL15.glBufferData(target, buffer, hint);
+					long offset = 0;
+					for (int i = 0; i < vertices.length; i++) {
+						if (vertices[i].length > 0) {
+							int dimensions = vertices[i][0].numDimensions();
+							if (dimensions <= 0 || dimensions > 4) {
+								throw new IllegalArgumentException("Dimensions can only be 1, 2, 3, or 4");
+							}
+							GL20.glVertexAttribPointer(i, dimensions, GL11.GL_FLOAT, false,
+									currentStride * BYTES_PER_FLOAT, offset * BYTES_PER_FLOAT);
+							offset += dimensions;
+						}
+					}
+				});
+				for (int i = 0; i < vertices.length; i++) {
+					GL20.glEnableVertexAttribArray(i);
+				}
+			});
 		}
 	}
 
-	private void initVertexArray(int[] indices, FloatData[][] vertices) {
-		vertexArray = new VertexArray();
-		vertexArray.bind(vao -> {
-			indexBuffer = new VertexBuffer();
-			indexBuffer.bind(GL15.GL_ELEMENT_ARRAY_BUFFER, (target, vbo) -> {
-				this.indexBufferSize = indices.length;
-				GL15.glBufferData(target, indices, hint);
-			}, false);
-			vertexBuffer = new VertexBuffer();
-			vertexBuffer.bind(GL15.GL_ARRAY_BUFFER, (target, vbo) -> {
-				FloatBuffer buffer = Drawable.getFloatBuffer(vertices, stride);
-				this.vertexBufferSize = buffer.capacity();
-				GL15.glBufferData(target, buffer, hint);
-				long offset = 0;
-				for (int i = 0; i < vertices.length; i++) {
-					if (vertices[i].length > 0) {
-						int dimensions = vertices[i][0].numDimensions();
-						if (dimensions <= 0 || dimensions > 4) {
-							throw new IllegalArgumentException("Dimensions can only be 1, 2, 3, or 4");
-						}
-						GL20.glVertexAttribPointer(i, dimensions, GL11.GL_FLOAT, false,
-								stride * BYTES_PER_FLOAT, offset * BYTES_PER_FLOAT);
-						offset += dimensions;
-					}
-				}
-			});
-			for (int i = 0; i < vertices.length; i++) {
-				GL20.glEnableVertexAttribArray(i);
-			}
-		});
-	}
-
-	public void setData(int[] indices, FloatData[][] vertices) {
-		this.numIndices = indices.length;
-		if (numIndices > 0) {
-			if (vertexArray == null) {
-				initVertexArray(indices, vertices);
-			} else {
+	public void setData(DrawableData data) {
+		// Technically we need to compare more than just the stride to adjust glVertexAttribPointer
+		// Also maybe need more glEnableVertexAttribArray
+		if (vertexArray == null) {
+			initVertexArray(data);
+		} else {
+			this.numIndices = data.indices().length;
+			if (numIndices > 0) {
+				var oldStride = currentStride;
+				currentStride = data.stride();
 				indexBuffer.bind(GL15.GL_ELEMENT_ARRAY_BUFFER, (target, vbo) -> {
-					if (indices.length > indexBufferSize) {
-						indexBufferSize = indices.length;
-						GL15.glBufferData(target, indices, hint);
+					if (numIndices > indexBufferSize) {
+						indexBufferSize = numIndices;
+						GL15.glBufferData(target, data.indices(), hint);
 					} else {
-						GL15.glBufferSubData(target, 0, indices);
+						GL15.glBufferSubData(target, 0, data.indices());
 					}
 				});
 				vertexBuffer.bind(GL15.GL_ARRAY_BUFFER, (target, vbo) -> {
-					int oldStride = stride;
-					stride = Drawable.getStride(vertices);
-					FloatBuffer newBuffer = Drawable.getFloatBuffer(vertices, stride);
+					var newBuffer = data.floatBuffer();
 					int newBufferSize = newBuffer.capacity();
 					if (newBufferSize > 0) {
 						if (newBufferSize > vertexBufferSize) {
@@ -93,14 +95,15 @@ public class DynamicIndexedDrawable implements Drawable {
 							GL15.glBufferSubData(target, 0, newBuffer);
 						}
 					}
-					if (stride != 0 && stride != oldStride) {
+					if (currentStride != 0 && currentStride != oldStride) {
 						vertexArray.bind(vao -> {
 							long offset = 0;
+							var vertices = data.vertices();
 							for (int i = 0; i < vertices.length; i++) {
 								if (vertices[i].length > 0) {
 									int dimensions = vertices[i][0].numDimensions();
 									GL20.glVertexAttribPointer(i, dimensions, GL11.GL_FLOAT, false,
-											stride * BYTES_PER_FLOAT, offset * BYTES_PER_FLOAT);
+											currentStride * BYTES_PER_FLOAT, offset * BYTES_PER_FLOAT);
 									offset += dimensions;
 								}
 							}
