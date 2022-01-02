c44a5b6164690ec5e63402620d6032f409b76fe3
awesomelemonade <awesomelemonade@gmail.com>
2022-01-01 21:41:46 -0500

    Reverse buttons

--- src/main/java/lemon/evolution/ui/beta/UIButtonList.java
+++ src/main/java/lemon/evolution/ui/beta/UIButtonList.java
@@ -29,7 +29,7 @@ public class UIButtonList extends AbstractUIChildComponent {
 			event.glfwWindow().pollMouse((mouseX, mouseY) -> {
 				if (buttonListBox.intersect(mouseX, mouseY)) {
 					scrollPercentage.setValue(MathUtil.saturate(
-							scrollPercentage.getValue() + (float) (event.yOffset() * 0.1f)));
+							scrollPercentage.getValue() + (float) (-event.yOffset() * 0.1f)));
 				}
 			});
 		}));
@@ -39,7 +39,7 @@ public class UIButtonList extends AbstractUIChildComponent {
 		var scrollBarHeight = 0.05f;
 		var disposeOnScroll = new Disposables();
 		// Scroll Bar Fixed GUI
-		children().add(new UIRenderable(this, () -> CommonRenderables.renderTransparentQuad2D(scrollBox, new Color(0.3f, 0.3f, 0.3f, 0.5f))));
+		children().add(new UIRenderable(this, () -> CommonRenderables.renderTransparentQuad2D(scrollBox, new Color(0f, 0f, 0f, 0.25f))));
 		// Mutable Components
 		disposables.add(scrollPercentage.onChangeAndRun(percentage -> {
 			disposeOnScroll.dispose();
@@ -47,9 +47,9 @@ public class UIButtonList extends AbstractUIChildComponent {
 			var layout = new GridLayout(buttonsBox, visibleButtons, 1, spacing);
 			var offsetButtons = (int) (percentage * (buttons.size() - visibleButtons));
 			for (int i = 0; i < visibleButtons; i++) {
-				var buttonInfo = buttons.get(offsetButtons + i);
+				var buttonInfo = buttons.get(offsetButtons + (visibleButtons - i) - 1);
 				var buttonBox = layout.getBox(i, 0);
-				var buttonTextBox = Box2D.ofInner(buttonBox, 0f, 0f, 8f, 5f);
+				var buttonTextBox = Box2D.ofInner(buttonBox, 0f, 0f, 10f, 8f);
 				mutableComponents.add(new UIButton(this, buttonBox, uiButton -> buttonInfo.runnable().run(), Color.WHITE));
 				mutableComponents.add(UIText.ofHeightCenterAligned(this, CommonFonts.freeSansTightened(), buttonInfo.text(), buttonTextBox, Color.BLACK));
 			}
@@ -60,12 +60,14 @@ public class UIButtonList extends AbstractUIChildComponent {
 			// Deconstruct components after update
 			disposeOnScroll.add(mutableComponents::clear);
 		}));
+		// Scroll Bar mouse events
 		var disposeOnRelease = new Disposables();
 		Consumer<Float> scrollBarUpdater = mouseY -> {
 			var scrollRangeHeight = scrollBox.height() - scrollBarHeight;
 			var newPercentage = 1.0f - (mouseY - scrollBox.y() - scrollBarHeight / 2.0f) / scrollRangeHeight;
 			scrollPercentage.setValue(MathUtil.saturate(newPercentage));
 		};
+		// On Press
 		disposables.add(input().mouseButtonEvent().add(mouseButtonEvent -> {
 			if (mouseButtonEvent.button() == GLFW.GLFW_MOUSE_BUTTON_1 && mouseButtonEvent.action() == GLFW.GLFW_PRESS) {
 				mouseButtonEvent.glfwWindow().pollMouse((pressX, pressY) -> {
@@ -79,6 +81,7 @@ public class UIButtonList extends AbstractUIChildComponent {
 				});
 			}
 		}));
+		// On Release
 		disposables.add(input().mouseButtonEvent().add(releaseButtonEvent -> {
 			if (releaseButtonEvent.button() == GLFW.GLFW_MOUSE_BUTTON_1 && releaseButtonEvent.action() == GLFW.GLFW_RELEASE) {
 				disposeOnRelease.dispose();
